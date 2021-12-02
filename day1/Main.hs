f6ac4f5bc1036ef3d6340fa4b1e8e5079f6cbc15
André Szabolcs Szelp <a.sz.szelp@gmail.com>
2021-12-02 09:20:29 +0100

    Day 1 update: make use of simple arithmetics
    This simplifies the solution, there is no need for `zip3`
    any more or that lambda, *and* the methdo becomes more general.

--- day1/Main.hs
+++ day1/Main.hs
@@ -7,31 +7,27 @@ main = do
         content <- readFile "input.txt"
         let d = (read <$> lines content) :: [Int]
         --
-        let increases1 = method1 d
-        print $ "Day 1, Puzzle 1, Version 1: " <> show increases1
-        let increases2 = method2 d
-        print $ "Day 1, Puzzle 1, Version 2: " <> show increases2
+        let increases1 = problem1 d
+        print $ "Day 1, Puzzle 1: " <> show increases1
         -- puzzle 2 of this day:
-        let slidingwindow = sliding d
-        print $ "Day 1, Puzzle 2, Version 2: " <> show slidingwindow
+        let slidingwindow = problem2 d
+        print $ "Day 1, Puzzle 2: " <> show slidingwindow
+
+-- For one, I have overlooked that (a + b + c) < (b + c + d) <==> a < d.
+-- with that, our solution can be reduced to one and the same problem
 
 -- this would actually be @(Ord b, Num a) => [b] -> a@,
 -- but we are restricting it for clarity
-method2 :: [Int] -> Int
-method2 d = fst $ foldl counter (0, head d) (tail d)
-  where
-      counter (i, old) next
-        | old < next = (i + 1, next)
-        | otherwise  = (i    , next)
-
-method1 :: [Int] -> Int
-method1 d = foldl counter 0 $ zip d (tail d)
+method :: Int -> [Int] -> Int
+method window d = foldl counter 0 $ zip d (drop window d)
   where
     counter :: Int -> Position -> Int
     counter i (a,b)
         | a < b     = i + 1
         | otherwise = i
 
-sliding :: [Int] -> Int
-sliding d = method2 sl
-  where sl = map (\(a,b,c) -> a + b + c) $ zip3 d (drop 1 d) (drop 2 d)
+problem2 :: [Int] -> Int
+problem2 = method 3
+
+problem1 :: [Int] -> Int
+problem1 = method 1
