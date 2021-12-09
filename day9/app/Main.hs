dcf03e62fcf428c228cfbc7ed678b12d6b7e64c3
André Szabolcs Szelp <a.sz.szelp@gmail.com>
2021-12-09 08:59:59 +0100

    Day 9, Problem 1

---
+++ day9/app/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import Common
+import Parse (parseLine)
+import Problem (problem1, problem2)
+
+main :: IO ()
+main = do
+  (input :: [String]) <- lines <$> readFile "data/input"
+  -- this time only the first line has data
+  let parsedInput = parseLine <$> input
+  -- let finalInput = preprocess parsedInput
+  let ly = length parsedInput
+  let lx = length $ head parsedInput
+  print (lx, ly)
+
+  let solution1 = problem1 parsedInput
+  putStrLn $ "Day 8, Problem 1: " <> show solution1
+  -- let solution2 = problem2 parsedInput
+  -- putStrLn $ "Day 8, Problem 2: " <> show solution2
---
+++ day9/src/Common.hs
@@ -0,0 +1,12 @@
+module Common (
+    Input,
+    Output,
+    Height,
+) where
+
+
+type Height = Int
+type Input = [[Height]]
+type Output = Int
+
+
