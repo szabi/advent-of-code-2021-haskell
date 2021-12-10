e3caa1e8303fc149c74ad2b343c78fe8e9e679da
André Szabolcs Szelp <a.sz.szelp@gmail.com>
2021-12-10 09:52:08 +0100

    Day 10, Problem 2

--- day10/app/Main.hs
+++ day10/app/Main.hs
@@ -2,7 +2,6 @@
 module Main where
 
 import Common
-import Parse (parseLine)
 import Problem (problem1, problem2)
 
 main :: IO ()
@@ -11,5 +10,5 @@ main = do
 
   let solution1 = problem1 input
   putStrLn $ "Day 10, Problem 1: " <> show solution1
-  -- let solution2 = problem2 parsedInput
-  -- putStrLn $ "Day 8, Problem 2: " <> show solution2
+  let solution2 = problem2 input
+  putStrLn $ "Day 10, Problem 2: " <> show solution2
--- day10/src/Problem.hs
+++ day10/src/Problem.hs
@@ -3,34 +3,56 @@
 module Problem (problem1, problem2) where
 
 import Common
-import GHC.IO.Handle.Text (commitBuffer')
-import Data.List (elemIndex)
-import Data.Maybe (catMaybes, fromJust, mapMaybe)
+import Data.List (elemIndex, sort)
+import Data.Maybe (fromJust, mapMaybe)
 
-problem2 = undefined
+problem2 :: [String] -> Int
+problem2 ss = scoreComplete'' $ scoreComplete' . completeStack <$> mapMaybe lineComplete ss
 
 problem1 :: [String] -> Int
 problem1 ss = sum (scoreError <$> mapMaybe lineError ss)
 
 type Stack = String
+type Complement = String
 
 opening = "([{<"
 closing = ")]}>"
 
-scoresError :: [Int]
-scoresError = [3,57,1197,25137]
+errorScores :: [Int]
+errorScores = [3,57,1197,25137]
 
 scoreError :: Char -> Int
-scoreError c | c `elem` closing = scoresError !! fromJust (elemIndex c closing)
-        | otherwise = error $ "score: invalid char: " <> show c
+scoreError = closing `mapTo` errorScores
+
+scoreComplete :: Char -> Int
+scoreComplete c = case elemIndex c closing of
+    Nothing -> error $ "scoresComplete: invalid char '" <> show c <> "', must be one of " <> show closing
+    Just n  -> n + 1
+
+scoreComplete' :: String -> Int
+scoreComplete' = foldl (\acc c -> acc * 5 + scoreComplete c) 0
+
+scoreComplete'' :: [Int] -> Int
+scoreComplete'' ns = sort ns !! (length ns `div` 2)
 
 lineError :: String -> Maybe Char
 -- returns Nothing if there is no stacking error,
 -- returns the violating character if there is a stacking error.
-lineError = leftToMaybe . foldl stack (Right "")
+lineError = leftToMaybe . buildStack
+
+lineComplete :: String -> Maybe String
+-- returns Nothing if there is a stack error,
+-- returns a `Just s` if there is a valid completion
+lineComplete = rightToMaybe . buildStack
+
+buildStack :: String -> Either Char Stack
+buildStack = foldl stack (Right "")
 
 leftToMaybe :: Either a b -> Maybe a
 leftToMaybe = either Just (const Nothing)
+
+rightToMaybe :: Either a b -> Maybe b
+rightToMaybe = either (const Nothing) Just
 
 stack :: Either Char Stack -> Char -> Either Char Stack
 stack l@(Left c) _ = l
@@ -42,3 +64,17 @@ stack (Right s) c'
                     then Right cs
                     else Left c'
         _        -> Left c'
+
+completeStack :: Stack -> Complement
+completeStack = foldl complete ""
+  where
+    complete :: Complement -> Char -> Complement
+    complete s c = s ++ [closingOf c]
+
+closingOf :: Char -> Char
+closingOf = opening `mapTo` closing
+
+mapTo :: (Eq a, Show a) => [a] -> [b] -> (a -> b)
+(source `mapTo` target) a
+    | a `elem` source = target !! fromJust (elemIndex a source)
+    | otherwise = error $ "invalid char: " <> show a <> "', must be one of " <> show source
